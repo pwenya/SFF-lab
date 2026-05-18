c987449be673b889c88146575633c96f1a0dcee0
pwenya <ruslanm652@gmail.com>
2026-05-18 19:17:35 +0300

    auto update 2026-05-18 19:17

--- api/payment/notify.js
+++ api/payment/notify.js
@@ -37,6 +37,7 @@ async function fetchPaymentStatus(paymentReference, credentials) {
 }
 
 module.exports = async function handler(req, res) {
+    if (req.method === 'GET') return res.status(200).json({ ok: true });
     if (req.method !== 'POST') return res.status(405).end();
 
     var payload = req.body || {};
